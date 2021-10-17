3af8e6813f4aa3071b4ac88db229cd37b2658b60
Hakan Ozler <ozler.hakan@gmail.com>
2021-10-17 09:44:00 +0300

    update the test case

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,30 @@
 import unittest
+
+import nbformat
+from nbconvert import NotebookExporter
+
 from pelikan import Pelikan
 
 
 class TestPelikanCases(unittest.TestCase):
 
-    def test_remove_commennts(self):
-        path = 'tests/data/'
+    def test_remove_comments(self):
+        path = 'data/'
         notebook = path + 'notebook-comments.ipynb'
         new_notebook_name = 'notebook-without-comments'
         file_extension = '.ipynb'
+        comment_free_notebooks = path + new_notebook_name + file_extension
 
         pelikan = Pelikan(notebook, new_notebook_name)
-        destination = pelikan.generate_notebook(path)
+        source_actual, resources_actual = pelikan._convert_comment()
 
-        self.assertIsNotNone(destination)
-        self.assertEqual("/".join(destination.parts), path + new_notebook_name + file_extension)
+        with open(comment_free_notebooks) as fh:
+            nb_comment_free = nbformat.reads(fh.read(), nbformat.NO_CONVERT)
+
+        exporter = NotebookExporter()
+        source_expected, resources_expected = exporter.from_notebook_node(nb_comment_free)
+
+        self.assertEqual(source_actual, source_expected)
 
 
 if __name__ == "__main__":
